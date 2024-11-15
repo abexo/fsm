52adce9ce16e03b644125246791cfbd11aecaee8
Panagiotis Spyropoulos <30686975+abexo@users.noreply.github.com>
2024-11-15 13:52:41 +0200

    Update move_state.gd

--- move_state.gd
+++ move_state.gd
@@ -9,6 +9,10 @@ func Enter():
 	animator.play("move")  # Start the move animation
 
 func Update(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	actor.velocity = direction * actor.speed
+	print(actor.velocity)
+	actor.move_and_slide()	
 	if actor.velocity == Vector2.ZERO:
 		transitioned.emit("IdleState")  # Transition to IdleState if not moving
 
