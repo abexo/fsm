e96650b698a1c82b9524b623fdda9109eec42069
Panagiotis Spyropoulos <30686975+abexo@users.noreply.github.com>
2024-11-15 13:52:08 +0200

    Update knight.gd

--- knight.gd
+++ knight.gd
@@ -1,12 +1,10 @@
 class_name Knight
 extends CharacterBody2D
 
-@export var max_speed = 40
+@export var speed = 60.0
 
 func _ready():
-	pass	
-	
+	pass  
+
 func _physics_process(delta: float) -> void:
- 	
-	velocity = Input.get_vector("move_left","move_right","move_up","move_down") * max_speed
-	move_and_slide()
+	pass
